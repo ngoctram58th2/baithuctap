6329555d595ee13c8a4e8a7a54e3f9ac6a3e270d
ngoctram58th2 <jennifervalley2512@gmail.com>
2018-12-25 08:53:28 +0700

    Update Bai1.cpp

--- Bai1.cpp
+++ Bai1.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include<string>
+#include<stdlib.h>
 using namespace std;
-int main()
+typedef struct NgayThang
 {
-	nt year;
-cout<<"Nhap nam: ";
-cin>>year;
-if( (year%400==0) || (year%4==0 && year%100!=0))
-cout<<"Nam "<<year<<" la nam nhuan";
-else cout<<"Nam "<<year<<" khong la nam nhuan";
+	int dd,mm,year;
+}NgayThang;
+void Nhap(string &st, NgayThang &Nt)
+{
+	cout <<"Nhap ngay thang nam:";
+	getline(cin,st);
+	cout<<st;
+}
+void LayDMY(string &s, NgayThang &ngay)
+{
+	ngay.dd= atoi((s.substr(0,2)).c_str());
+	cout<<ngay.dd<<endl;
+	ngay.mm = atoi((s.substr(3,2)).c_str());
+	cout<<ngay.mm<<endl;
+	ngay.year = atoi((s.substr(6,4)).c_str());
+	cout<<ngay.year<<endl;
+}
+int main()
+{	
+	struct NgayThang ngay;
+	string s;
+	Nhap(s,ngay);
+
+	LayDMY(s,ngay);
+	cout<<endl;
 }
