de3417562737bb3bde8f19084e0d0bef1ab6cd41
ngoctram58th2 <jennifervalley2512@gmail.com>
2018-12-25 21:29:02 +0700

    Update Bai1.cpp

--- Bai1.cpp
+++ Bai1.cpp
@@ -7,13 +7,20 @@ typedef struct NgayThang
 	int dd,mm,year;
 
 }NgayThang;
+
+void chuoi( string st,NgayThang &x);
+int NamNhuan(int year);
+int stt(NgayThang x);
+int month[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+NgayThang NgayTuSTT(int stt, int year);
+NgayThang congNgayThang(NgayThang x, int a)
 void Nhap(string &st, NgayThang &Nt)
 {
 	cout <<"Nhap ngay thang nam:";
 	getline(cin,st);
 
 }
-void LayDMY(string &s, NgayThang &ngay)
+void chuoi(string &s, NgayThang &ngay)
 {
 	ngay.dd= atoi((s.substr(0,2)).c_str());
 	cout<<ngay.dd<<endl;
@@ -31,12 +38,12 @@ int ktranhuan(NgayThang ngay)
 }
 int TimNN(NgayThang n)
 {
-	int std=0;
+	int stt=0;
 	for(int i=0;i<n.mm;i++)
-		std += n.dd;
+		stt += n.dd;
 		if(ktranhuan) std+1;
-		else std;
-NgayTuSTT(int stt, int year)
+		else stt;
+int TongNgayThang(int stt, int year)
 {
 int kq= a + sttNgayTrongNam(x);
 	int year=x.year;
@@ -52,16 +59,75 @@ int kq= a + sttNgayTrongNam(x);
 				return 	NgayTuSTT(kq-365,year+1);	
 }
 
+NgayThang NgayTuSTT(int stt, int year){
+	int i=1;
+	int ngay=stt;
+	int kt =NamNhuan(year);
+	while(stt > 0){
+		if((kt == 1) && (i==2))
+			stt = stt-29;
+		else stt = stt- month[i];
+		if(stt > 0)
+		{
+			ngay= stt;
+			i++;
+		}	
+		ngaythang x;
+		x.dd=ngay;
+		x.mm=i;
+		x.year= year;
+		return x;			
+	}	
+}
+int stt(NgayThang x)
+{
+	int stt=0;
+	for(int i =0; i< x.month; i++)
+	stt+=month[i];
+	stt += x.day;
+	if(NamNhuan==0 && x.month>2) stt+=1;
+	return stt;
+
+}
+
+void Cong(NgayThang a, int &x)
+{
+	
+	cout << "\nnhap so cong them: ";
+	cin >> x;
+	
+	int n = STTday(a) + x;
+	
+	NgayThang b;
+	b.year = a.year;
+	b.month = 1;
+	b.day = 0;
+	
+	while(n > 365)
+		for(n; n >= 366; n-=366)
+		{
+			if(KTyear(b) == 0)
+				n++;
+			b.year++; 
+		}
+	if(n == 365)
+	{
+		b.year++; 
+		b.day = 1; 
+		b.month = 1; 
+	}
+	else
+		if(n < 365)
+			STTSangNgay(b, n); 
+}
+-
 int main()
 {	
 	struct NgayThang ngay;
 	string s;
 	
 	Nhap(s,ngay);
-	LayDMY(s,ngay);
+	chuoi(s,ngay);
 	cout<<endl;
-	if(ktranhuan(ngay)) 
-		cout << "Nam nhuan";
-	else
-		cout << "Khong nhuan";
+
 }
