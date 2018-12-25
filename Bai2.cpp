249a2ef82c489a15ed9e27070ca63bf718d87168
ngoctram58th2 <jennifervalley2512@gmail.com>
2018-12-25 23:40:25 +0700

    Update Bai2.cpp

--- Bai2.cpp
+++ Bai2.cpp
@@ -8,7 +8,7 @@ typedef struct NgayThang
 	int mm;
 	int year;
 }NgayThang;
-
+int month[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 void Nhap(string &stt, NgayThang &date)
 {
 	cout << "nhap du lieu: (phan biet boi dau /)"; 
@@ -26,69 +26,81 @@ void chuoi(string &st, NgayThang &date)
 	cout << "nam: " << date.year <<endl;
 }
 
-int ktnhuan (NgayThang ngay)
+int ktnhuan (int year)
 {
-	 if(((ngay.year)%100 == 0) && ((ngay.year)%400 == 0) || (((ngay.year)%100 != 0) && ((ngay.year)%4 == 0)))
+	 if(((year)%100 == 0) && ((year)%400 == 0) || (((year)%100 != 0) && ((year)%4 == 0)))
         return 1;
     else
         return 0;
 }
 
-int NgayTungThang(NgayThang date, int i)
+
+int STT(NgayThang date)
 {
-	int SoNgay;
-	
-	switch (i)
-	{
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12: 
-			SoNgay = 31;
-			break;
-		case 4:
-		case 6:
-		case 9:
-		case 11: 
-			SoNgay = 30;
-			break;
-		case 2:
-			if (ktnhuan(date) == 1)
-			{
-				SoNgay = 29;
-			}
+	int STT=0;
+	for(int i =0; i< date.mm; i++)
+	STT += month[i];
+	STT += date.dd;
+	if(ktnhuan==0 && date.mm > 2) STT +=1;
+	return STT;
+
+}
+
+NgayThang NgayTuSTT(int n,int year)
+{
+	int i=0;
+	int ngay= n;
+	int kt = ktnhuan(ngay);
+	while(n >0)
+		{
+			if((kt==1) && (i==2))
+				n = n -29;
 			else
-			{
-				SoNgay = 28;
-			}
-			break;
-	}
-
-	return SoNgay;	
+				n = n - month[i];
+			if(n>0)
+				{
+					ngay = n;
+					i++;
+				}
+		}
+	NgayThang nt;
+	nt.dd = ngay;
+	nt.mm = i;
+	nt.year = year;
+	return nt;
 }
-
-int NgayTuSTT(NgayThang date)
+NgayThang TongNgayThang(NgayThang n,int x) //x,a
 {
-	int ngay = date.dd;
-	for(int i = 1; i <= date.mm - 1; i++)
-	{
-		ngay = ngay + NgayTungThang(date, i);
-	}
-	return ngay;
+	
+	int kq= x + STT(n);
+	int year=n.year;
+	if(x <= 365)
+	if(kq <= 365)
+		return NgayTuSTT(kq,year);
+	else
+		if(ktnhuan(year)==1)
+			if(kq == 366)
+				return NgayTuSTT(kq,year);
+			else
+				return NgayTuSTT(kq-366,year+1);
+		else
+			return 	NgayTuSTT(kq-365,year+1);
+
+	else
+		if(ktnhuan(year)==1)
+			return NgayTuSTT(kq+366,year+1);
+		else
+			return NgayTuSTT(kq+365,year+1); 
 }
-
-
 int main()
 {
 	struct NgayThang ngay;
 	string st;
+	int x;
 	Nhap(st, ngay);
 	chuoi(st, ngay);
-	if(ktnhuan(ngay) == 1 ) cout << ngay.year << " la nam nhuan";
-	else cout << ngay.year << " khong phai nam nhuan";
-	cout << "\nSTT ngay trong nam la: " << NgayTuSTT(ngay);
-	
+	cout << "\nSTT ngay trong nam la: " << STT(ngay);
+	cout<<"\nNhap vao so nguyen can tinh: ";
+	cin>> x;
+	TongNgayThang(ngay,x);
 }
