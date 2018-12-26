130766bebd29a2b24429fe3511b62ec4ac1c0aae
ngoctram58th2 <jennifervalley2512@gmail.com>
2018-12-27 00:18:14 +0700

    Update Bai1.cpp

--- Bai1.cpp
+++ Bai1.cpp
@@ -5,7 +5,8 @@
 #include<fstream>
 #include<vector>
 #include<string>
-
+#include<windows.h>
+#include <cstdlib>
 using namespace std;
 
 
@@ -15,58 +16,91 @@ typedef struct Contact
 	char sdt[11];
 	char gmail[20];
 	char diachi[50];
-	bool gioitinh;	
+	bool gioitinh;
 }Contact;
-void Nhap(Contact c)
+void Nhap()
 {
-	cout <<"0168546321"<<c.sdt<<"\n";
-	cout<<"Ten"<<c.ten<<"\n";
+	Contact c;
+	strcpy(c.ten,"abc");
+	strcpy(c.sdt,"4564645");
+	strcpy(c.gmail,"abcs@gmail.com");
+	strcpy(c.diachi,"ryhr64465");
 }
 vector<Contact> db;
-void GhiVaoDB()
+
+void GhiVaoFile()
 {
-	
-	int size = db.size();
-	FILE * f=fopen("db.dat","wb");
+
+	FILE*f=fopen("db.dat","wb");
 	Contact c;
-	for(int i=0; i <size; i++)
-		{
-			c=db[i];
-			fwrite(&c, sizeof(Contact),1,f);
-		}
+	for(int i = 0 ; i < db.size() ; i++)
+		fwrite(&db[i],sizeof(db),1,f);	
 		fclose(f);
 }
+
 void DocFile()
 {
 	db.clear();
-	FILE*f;
-	f=fopen("db.dat","rb");
-	Contact c;
-	if(f!=NULL)
+	FILE*f =fopen("db.dat","rb");
+	if( f != NULL)
 	{
-		while(!feof(f))
-		{
-			fread(&c,sizeof(Contact),1,f);
+		while(!feof(f)){
+			Contact c;
+			fread(&c,sizeof(Contact),1,f);	
 			db.push_back(c);
 		}
-		fclose(f);
-		db.pop_back();
-			
-	}
-}
-void ThemMoi(Contact c)  
-{	
-	  db.push_back(c);
-	  GhiVaoDB();
+			fclose(f);
+			db.pop_back();
+	}	
 }
 
 
-/*void LietKe()
+void ThemDB(Contact c)
 {
-	DocFile();
-	for (int i = 0; i<sizeof(db);i++)
-	Xuat(db[i]);
+	cout<<"Them danh ba\n";
+	cout<<"So dien thoai: ";
+	cin.getline(c.sdt,11);
+	cout<<"Ten: ";
+	cin.getline(c.ten,20);
+	cout<<"Gmail: ";
+	cin.getline(c.gmail,20);
+	cout<<"Dia chi: ";
+	cin.getline(c.diachi,50);
+	db.push_back(c);
 }
+
+
+void LietKe()
+	{
+	Contact c;
+	for (int i = 0; i<sizeof(db);i++)
+		{
+		  cout << "\nSo dien thoai: "<<c.sdt<<endl;
+		  cout << "Ten: "<<c.ten<<endl;
+		  cout << "Gmail: "<<c.gmail<<endl;
+		  cout << "Dia chi: "<<c.diachi<<endl;
+		}
+	}
+	
+void ThemMoi(Contact c)
+{
+	db.push_back(c);
+	GhiVaoFile();
+}
+
+void CapNhat(Contact c)
+{
+	FILE*f=fopen("db.dat","wb");
+//	Contact cmoi;
+	ThemMoi(c);
+	LietKe();
+	for(int i = 0 ; i < sizeof(db) ; i++)
+		fwrite(&db[i],sizeof(db),1,f);	
+		fclose(f);
+}
+
+
+
 /*them moi contact vao dl
 //them vao du lieu
 //ghi du lieu vao file
@@ -87,9 +121,26 @@ TimTheoTen(char*ten);
 //*/
 int main()
 {
-	Contact c;
-	Nhap(c);
 	DocFile();
-	GhiVaoDB();
+	LietKe();
+	Contact c;
+		strcpy(c.ten,"pipi");
+		strcpy(c.sdt,"2656565400");
+		strcpy(c.gmail,"asdas@gmail.com");
+		strcpy(c.diachi,"fafafadfa");
 	ThemMoi(c);
+	Contact c1;
+		strcpy(c1.ten,"pipivdzvs");
+		strcpy(c1.sdt,"265650056565");
+		strcpy(c1.gmail,"asdaxzzs@gmail.com");
+		strcpy(c1.diachi,"nha trang");
+	ThemMoi(c1);
+	cout<<"DB";
+	LietKe();
+	Contact c2;
+	ThemDB(c2);
+	cout<<"Cap nhat";
+	CapNhat(c2);
+	GhiVaoFile();
 }
+	
