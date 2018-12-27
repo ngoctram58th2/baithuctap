f300d9861a368a7425cfaf91cb249bdcd553ec56
ngoctram58th2 <jennifervalley2512@gmail.com>
2018-12-27 12:50:05 +0700

    Update Bai1.cpp

--- Bai1.cpp
+++ Bai1.cpp
@@ -16,31 +16,54 @@ typedef struct Contact
 	char sdt[11];
 	char gmail[20];
 	char diachi[50];
-	bool gioitinh;
 }Contact;
-vector<Contact>db; 
+vector<Contact>db;
+ 
+void GhiVaoFile();
+void Them(Contact c);
+void DanhBa();
+void DocFile();
+void ThemDB(Contact c);
+void LietKe();
+void Xuat(Contact c);
+void CapNhat();
+void TimTheoTen(char t[]);
 
-void DanhBa(Contact c)
+
+void GhiVaoFile()//ghi du lieu vao file
 {
- 	strcpy(c.ten,"hfgh");
-	strcpy(c.sdt,"46564");
-	strcpy(c.gmail,"rtyrty");
-	strcpy(c.diachi,"fafafadfa");
-	db.push_back(c);
-}
-
-void GhiVaoFile()
-{
-
-	int size = db.size();
-	FILE * f=fopen("db.dat","wb");
+	FILE * f=fopen("db.dat","wb");//mo file len
 	Contact c;
-	for(int i=0; i <size; i++)
+	for(int i=0; i < db.size(); i++)
 		{
 			c=db[i];
 			fwrite(&c, sizeof(Contact),1,f);
 		}
 		fclose(f);
+}
+void Them(Contact c) //them vao du lieu va xuat ra man hinh
+{
+	  db.push_back(c);
+	  GhiVaoFile();
+}
+void DanhBa()
+{
+	Contact c,c1;
+ 	strcpy(c1.ten,"bo");
+	strcpy(c1.sdt,"054365465");
+	strcpy(c1.gmail,"bo@gmail.com");
+	strcpy(c1.diachi,"59 Nguyen Dinh Chieu");
+	Them(c1);
+	strcpy(c.ten,"uyen phuong");
+	strcpy(c.sdt,"035463214");
+	strcpy(c.gmail,"phuong@gmail.com");
+	strcpy(c.diachi,"59 Nguyen Dinh Chieu");
+	Them(c);
+	strcpy(c.ten,"thu");
+	strcpy(c.sdt,"06435345");
+	strcpy(c.gmail,"thu@gmail.com");
+	strcpy(c.diachi,"24 Doan Tran Nghiep");
+	Them(c);
 }
 
 void DocFile()
@@ -55,22 +78,36 @@ FILE *f = fopen("db.dat","rb");
 	fclose(f);
 	}
 
+void Xoa(char sd[])
 
-void ThemDB(Contact c)
+{
+for(int i=0;i<db.size();i++)
+			if(strcmp(db[i].sdt,sd)==0)// so sanh sdt can tim voi sdt trong danh ba
+				db.erase(db.begin()+i);//xoa db da tim duoc
+	cout<<"Cap nhat lai danh ba sau khi xoa: ";
+	LietKe();	//xuat ra cac danh ba con lai
+}
+
+void ThemDB(Contact c)//them vao 1 danh ba
 {
 
-	strcpy(c.ten,"fsdfsd");
-	strcpy(c.sdt,"423535");
-	strcpy(c.gmail,"rtyrty");
-	strcpy(c.diachi,"fafafadfa");
-	db.push_back(c);
+	cout<<"Them danh ba\n";
+	cout<<"So dien thoai: ";
+	cin.getline(c.sdt,11);
+	cout<<"Ten: ";
+	cin.getline(c.ten,20);
+	cout<<"Gmail: ";
+	cin.getline(c.gmail,20);
+	cout<<"Dia chi: ";
+	cin.getline(c.diachi,50);
+	Them(c);
 }
 
 
 void LietKe()
 	{
 		
-	for (int i = 0; i<db.size();i++)
+	for (int i = 0; i<db.size();i++) //vong lap for 
 		{
 		  cout << "\nSo dien thoai: "<<db[i].sdt<<endl;
 		  cout << "Ten: "<<db[i].ten<<endl;
@@ -78,19 +115,20 @@ void LietKe()
 		  cout << "Dia chi: "<<db[i].diachi<<endl;
 		}
 	}
-//them moi contact vao dl
-void ThemMoi(Contact c)
+	
+void Xuat(Contact c)
 {
-	  db.push_back(c);
-	  GhiVaoFile();
+		cout<<"Ten: "<<c.ten<<endl;
+		cout<<"So dien thoai: "<<c.sdt<<endl;
+		cout<<"Dia chi: "<<c.diachi<<endl;
+		cout<<"Gmail: "<<c.gmail<<endl;
 }
+//them moi contact vao dl
 void CapNhat() 
-//void CapNhat(Contact c);
-//tim contact, co sdt
-//cap nhat c vao m
+//cap nhat them vao
 {
 	Contact c;
-	FILE*f=fopen("db.dat","wb");
+	FILE*f=fopen("db.dat","wb");//mo file len
 	cout<<"\n------------------------------\n";
 	cout <<"Danh ba sau khi cap nhat";
 	LietKe();
@@ -99,7 +137,12 @@ void CapNhat()
 		fclose(f);
 }
 
-
+void TimTheoTen(char t[])
+{
+	for(int i=0;i<db.size();i++)
+			if(strcmp(db[i].ten,t)==0)// so sanh de tim ten trong danh sach voi ten nhap
+				Xuat(db[i]);
+}
 
 /*
 //them vao du lieu
@@ -118,13 +161,16 @@ TimTheoTen(char*ten);
 int main()
 {
 	Contact c,c1;
-	strcpy(c.ten,"hfgh");
-	strcpy(c.sdt,"46564");
-	strcpy(c.gmail,"rtyrty");
-	strcpy(c.diachi,"fafafadfa");
-	ThemMoi(c);
-	ThemDB(c1);
-	LietKe();
+	//Danh ba co san
+	ThemDB(c);//them 1 danh ba moi
+	DanhBa();
 	CapNhat();
+	cout<<"-----------------------------\n";
+	cout<<"Tim kiem theo ten(bo): "<<endl;
+	TimTheoTen("bo");
+	cout<<"-----------------------------\n";
+	cout<<"Xoa danh ba co sdt 06435345\n";
+	Xoa("06435345");
+	
 }
 	
