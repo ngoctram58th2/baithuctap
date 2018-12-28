448208fc7e004f1a4da967fcd88fa4ae075f76ed
ngoctram58th2 <jennifervalley2512@gmail.com>
2018-12-28 09:40:29 +0700

    Update bai4.cpp

--- bai4.cpp
+++ bai4.cpp
@@ -19,22 +19,38 @@ void XuatDoThi(int a[10][10],int n)
 		for(int i = 0; i < n; i++)
 		{
 		for(int j=0;j < n;j++)
-		
 			cout << a[i][j]<<" ";
 			cout<<endl;
 		}
 	}
-void TaoMoi(int a[10][10], int n)
+void TaoMoi(int a[10][10], int &n)
 {
-	cout<<"Nhap"
+	cout<<"Nhap n: ";
+	cin>>n;
+	fi.open("dothi.txt",ios::out);
+	fi<<n<<endl;
+	for(int i=0;i<n; i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cout<<"Hang so "<<i<<" cot " << j <<" :";
+			cin >> a[i][j];
+			fi<<a[i][j]<<" ";
+		}
+		cout<<endl;
+		fi<<endl;
+	}
+	fi.close();
 }
 
 int main()
 {
 	int n;
-	fi.open("dothi.txt",ios::in);
-	fi>>n;
 	int a[10][10];
+	TaoMoi(a,n);
+	fi.open("dothi.txt",ios::in);
+	fi >> n;
 	DocFile(a,n);
 	XuatDoThi(a,n);
+	fi.close();
 }
