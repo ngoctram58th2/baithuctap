1de269f6b6f4d630bd5e7414cb276aba356a6b37
ngoctram58th2 <jennifervalley2512@gmail.com>
2019-01-20 01:14:18 +0700

    Update Bai2.cpp

--- Bai2.cpp
+++ Bai2.cpp
@@ -11,19 +11,9 @@ typedef struct NgayThang
 int month[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 void Nhap(string &stt, NgayThang &date)
 {
-	cout << "nhap du lieu: (phan biet boi dau /): "; 
+	cout << "Nhap ngay thang nam: (phan biet boi dau /, ngay thang 2 chu so): "; 
 	getline(cin, stt);
 	cout << "ngay thang vua nhap: " << stt;
-}
-
-void chuoi(string &st, NgayThang &date)
-{
-	date.dd = atoi((st.substr(0,2)).c_str());
-	cout << "\nngay: " << date.dd<<endl;
-	date.mm = atoi((st.substr(3,2)).c_str());
-	cout << "thang: " << date.mm<<endl;
-	date.year = atoi((st.substr(6,4)).c_str());
-	cout << "nam: " << date.year <<endl;
 }
 
 int ktnhuan (int year)
@@ -33,6 +23,22 @@ int ktnhuan (int year)
     else
         return 0;
 }
+
+void chuoi(string &st, NgayThang &date)
+{
+	date.dd = atoi((st.substr(0,2)).c_str());//cat chuoi lay ra 2 ky tu
+	cout << "\nngay: " << date.dd<<endl;
+	date.mm = atoi((st.substr(3,2)).c_str());//cat chuoi lay ra 2 ky tu tiep theo tu ky tu thu 3
+	cout << "thang: " << date.mm<<endl;
+	date.year = atoi((st.substr(6,4)).c_str());//cat chuoi lay ra 4 ky tu tiep theo tu ky tu thu 6
+	cout << "nam: " << date.year <<endl;
+	if(ktnhuan(date.year))
+		cout<<"La nam nhuan";
+	else
+		cout<<"Khong phai nam nhuan";
+}
+
+
 
 
 int STT(NgayThang date)
@@ -68,29 +74,30 @@ NgayThang NgayTuSTT(int n,int year)
 	nt.mm = i;
 	nt.year = year;
 	return nt;
-}
+}	
+
 NgayThang TongNgayThang(NgayThang n,int x)
 {
 	
 	int kq= x + STT(n);
 	int year=n.year;
-	if(x <= 365)
-		if(kq <= 365)
-			return NgayTuSTT(kq,year);
-		else
-			if(ktnhuan(year)==1)
-				if(kq == 366)
-					return NgayTuSTT(kq,year);
-				else
-					return NgayTuSTT(kq-366,year+1);
-			else
-				return 	NgayTuSTT(kq-365,year+1);
+	if(x <= 365)//neu so nguyen duong nhap tu ban phim nho hon 365
+		if(kq <= 365) //neu kq nho hon 365
+			return NgayTuSTT(kq,year); //tra ve kq
+		else //neu kq <365
+			if(ktnhuan(year)==1)//la nam nhuan
+				if(kq == 366)//neu kq=366
+					return NgayTuSTT(kq,year);//tra ve kq
+				else//kq >366
+					return NgayTuSTT(kq-366,year+1);//lay kq-365, nam tang them 1
+			else//khong la nam nhuan
+				return 	NgayTuSTT(kq-365,year+1);//lay kq-365, nam tang them 1
 	
-	else
-		if(ktnhuan(year)==1)
-			return NgayTuSTT(kq+366,year+1);
+	else//neu so nguyen duong lon hon 365
+		if(ktnhuan(year)==1)//la nam nhuan
+			return NgayTuSTT(kq+366,year+1);//kq cong them 366 va nam tang them 1
 		else
-			return NgayTuSTT(kq+365,year+1);
+			return NgayTuSTT(kq+365,year+1);//kq cong them 365 va nam tang them 1
 }
 
 NgayThang TruNgayThang(NgayThang n,int x)
@@ -110,5 +117,5 @@ int main()
 	cout << "\nSTT ngay trong nam la: " << STT(ngay);
 	cout<<"\nNhap vao so nguyen can tinh: ";
 	cin>> x;
-	cout<<TongNgayThang(ngay,x).dd<<"/"<<TongNgayThang(ngay,x).mm<<"/"<<TongNgayThang(ngay,x).year;
+	cout<<"Ngay thang nam sau khi cong voi so "<<x<<" la: "<<TongNgayThang(ngay,x).dd<<"/"<<TongNgayThang(ngay,x).mm<<"/"<<TongNgayThang(ngay,x).year;
 }
